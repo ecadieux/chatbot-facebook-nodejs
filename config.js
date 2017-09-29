8e082a7ef3cb06b9375e969ac29289f4f7993ff4
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-09-29 11:20:53 -0400

    Added config file information for MustiRobot

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAbvysHaCg4BACgrO48oEKrrx9uhguUPwZAWOrZC65F3T38mtFTEsJEHARYu9eaJIZCmvSDgU88ycukNyMZCZAT4Sc3qRoCgMQtSSFHkEXjL7ZAwvWRxyxNRZCma2NlXR25emgIy1G1vaMPyF9ReZCg6c5Uj8yGqczWsod4CA7lB7gZDZD',
+    FB_VERIFY_TOKEN: 'musti_robot_verify_token',
+    API_AI_CLIENT_ACCESS_TOKEN: 'dc8022531f164f47bf0b20994a8f7627',
+    FB_APP_SECRET: 'eb2c63ccf1e19491ccbfcee6e3e8cf58',
+    SERVER_URL: "https://musti-robot.herokuapp.com/",
 };
