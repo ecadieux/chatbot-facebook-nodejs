92f813546ce3d6dd1be7c4ea57ad7afc29fd6397
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-09-29 18:51:44 -0400

    Implemented Heroku env variables

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAbvysHaCg4BACgrO48oEKrrx9uhguUPwZAWOrZC65F3T38mtFTEsJEHARYu9eaJIZCmvSDgU88ycukNyMZCZAT4Sc3qRoCgMQtSSFHkEXjL7ZAwvWRxyxNRZCma2NlXR25emgIy1G1vaMPyF9ReZCg6c5Uj8yGqczWsod4CA7lB7gZDZD',
-    FB_VERIFY_TOKEN: 'musti_robot_verify_token',
-    API_AI_CLIENT_ACCESS_TOKEN: 'dc8022531f164f47bf0b20994a8f7627',
-    FB_APP_SECRET: 'eb2c63ccf1e19491ccbfcee6e3e8cf58',
-    SERVER_URL: "https://musti-robot.herokuapp.com/",
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'frank@alpacode.com',
-    EMAIL_TO: 'eric.c@alpacode.com'
+    FB_PAGE_TOKEN: process.env.HEROKU_FB_PAGE_TOKEN,
+    FB_VERIFY_TOKEN: process.env.HEROKU_FB_VERIFY_TOKEN,
+    API_AI_CLIENT_ACCESS_TOKEN: process.env.HEROKU_API_AI_CLIENT_ACCESS_TOKEN,
+    FB_APP_SECRET: process.env.HEROKU_FB_APP_SECRET,
+    SERVER_URL: process.env.HEROKU_SERVER_URL,
+    SENDGRID_API_KEY: process.env.HEROKU_SENDGRID_API_KEY,
+    EMAIL_FROM: process.env.HEROKU_EMAIL_FROM,
+    EMAIL_TO: process.env.HEROKU_EMAIL_TO
 };
