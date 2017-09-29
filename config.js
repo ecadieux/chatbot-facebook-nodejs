ea65d0f4afe63adc143f577aa53b1b55939c71f9
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-09-29 17:51:11 -0400

    Added Sendgrid configuration variables

--- config.js
+++ config.js
@@ -4,4 +4,7 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: 'dc8022531f164f47bf0b20994a8f7627',
     FB_APP_SECRET: 'eb2c63ccf1e19491ccbfcee6e3e8cf58',
     SERVER_URL: "https://musti-robot.herokuapp.com/",
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'frank@alpacode.com',
+    EMAIL_TO: 'eric.c@alpacode.com'
 };
