9abcf6736dc17bff93d120e09856a5efa34c7821
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-02 18:44:56 -0400

    Tried to troubleshoot user.js

--- user.js
+++ user.js
@@ -18,49 +18,43 @@ module.exports = function(callback,userId){
       var user = JSON.parse(body);
 
       if (user.first_name) {
-        console.log('First name correctly fetched from user object.');
 
-        var pool = new pg.Pool(config.PG_CONFIG);
-          pool.connect(function(err, client, done) {
-            if (err) {
-              return console.error('Error acquiring client', err.stack);
-            }
-            var rows = [];
-            console.log('fetching user');
-            client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
-              function(err, result) {
-                console.log('query result ' + result);
-                if (err) {
-                  console.log('Query error: ' + err);
-                } else {
-                  console.log('rows: ' + result.rows.length);
-                  if (result.rows.length === 0) {
-                    let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
-                      'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-                    console.log('sql: ' + sql);
-                    client.query(sql,
-                      [
-                        userId,
-                        user.first_name,
-                        user.last_name,
-                        user.profile_pic,
-                        user.locale,
-                        user.timezone,
-                        user.gender
-                      ]);
-                  }
-                }
-              });
+                            var pool = new pg.Pool(config.PG_CONFIG);
+                            pool.connect(function(err, client, done) {
+                            	if (err) {
+                            		return console.error('Error acquiring client', err.stack);
+                            	}
+                            	var rows = [];
+                            	console.log('fetching user');
+                            	client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+                            		function(err, result) {
+                            			console.log('query result ' + result);
+                            			if (err) {
+                            				console.log('Query error: ' + err);
+                            			} else {
+                            				console.log('rows: ' + result.rows.length);
+                            				if (result.rows.length === 0) {
+                            					let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
+                            						'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+                            					console.log('sql: ' + sql);
+                            					client.query(sql,
+                            						[
+                            							userId,
+                            							user.first_name,
+                            							user.last_name,
+                            							user.profile_pic,
+                            							user.locale,
+                            							user.timezone,
+                            							user.gender
+                            						]);
+                            				}
+                            			}
+                            		});
+                                done();
+                                callback(user);
 
-                callback(user);
-          });
-          pool.end();
-
-        console.log("FB user: %s %s, %s",
-          user.first_name, user.last_name, user.gender);
-
-
-
+                            });
+                            pool.end();
 
 
       } else {
