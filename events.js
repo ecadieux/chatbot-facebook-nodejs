a2545d869751ccc6de67bb2b8d76196a6f7ccc6e
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-06 03:59:54 -0400

    Attempt to get api.ai sessionid

--- events.js
+++ events.js
@@ -14,10 +14,28 @@ pg.defaults.ssl = true;
 // for api.ai webhooks
 
 app.get('/webhook-apiai/', function (req, res) {
-	console.log("webhook-apiai request":+req);
-  console.log("This is webhook-apiai's result: "+res);
-
+	var sessionID = req.sessionId;
 
 	res.status(200);
-	res.send("This seems to work");
+	res.send("This seems to work :"+sessionID);
 	})
+
+
+// Event caller
+/*
+  app.get('https://api.api.ai/v1/query', function (req, res) {
+
+
+  var v = req.param('20150910');
+  var e = req.param('1_jour');
+  var timezone = req.param('America/Barbados');
+  var lang = req.param('fr');
+  var sessionId = req.param();
+
+  res.setHeader("Authorization: Bearer "+confi.API_AI_CLIENT_ACCESS_TOKEN);
+
+
+  	res.status(200);
+  	res.send(v+' '+e+' '+timezone+' '+lang);
+  	})
+*/
