cb14101d47e5db64c5abc3a9df947107307c9c3e
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-06 06:01:55 -0400

    day1.js

--- day1.js
+++ day1.js
@@ -1,25 +1,10 @@
+
 'use strict';
-//const request = require('request');
-const config = require('./config');
-const pg = require('pg');
-const apiai = require('apiai');
-const express = require('express');
-const crypto = require('crypto');
-//const bodyParser = require('body-parser');
-//const app = express();
-const uuid = require('uuid');
-const mainApp = require('./app');
+const config = require("./config");
 var util = require('util');
-
-pg.defaults.ssl = true;
-
-
-mainApp.sendTextMessage(945859885542482,'This is a test for schedule day one.');
-console.log("Sending scheduled message");
-
-
+// var apiai = require("../module/apiai");
+var apiai = require("apiai");
 var APIAIsessionID = uuid.v1();
-
 var options = {
     hostname: 'https://api.api.ai/v1/query',
 };
@@ -28,13 +13,11 @@ var app = apiai(config.API_AI_CLIENT_ACCESS_TOKEN, options);
 
 var event = {
     name: "1_jour",
-    data: {
-        param1: "",
-    }
+
 };
 
 var options = {
-    sessionId: 'APIAIsessionID'
+    sessionId: APIAIsessionID
 };
 
 var request = app.eventRequest(event, options);
