b0973638393b1043e09de3216951a89368731b82
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-06 06:48:13 -0400

    ...

--- day1.js
+++ day1.js
@@ -2,40 +2,6 @@
 'use strict';
 const config = require("./config");
 const uuid = require('uuid');
-var util = require('util');
-// var apiai = require("../module/apiai");
-var apiai = require("apiai");
-var APIAIsessionID = uuid.v1();
+const mainApp = require("./app");
 
-
-module.exports = function(app){
-
-
-var options = {
-    hostname: 'https://api.api.ai/v1/query',
-};
-
-
-var app = apiai("'"+config.API_AI_CLIENT_ACCESS_TOKEN+"'", options);
-
-var event = {
-    name: "1_jour",
-
-};
-
-var options = {
-    sessionId: APIAIsessionID
-};
-
-var request = app.eventRequest(event, options);
-
-request.on('response', function(response) {
-    console.log(util.inspect(response, false, null));
-});
-
-request.on('error', function(error) {
-    console.log(error);
-});
-
-request.end();
-}
+mainApp.sendTextMessage(945859885542482,'This is a test.');
