a363ce02f3b403eb50e3e332a88a6eec507307b6
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-06 04:51:58 -0400

    Testing scheduler

--- day1.js
+++ day1.js
@@ -8,9 +8,9 @@ const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const app = express();
 const uuid = require('uuid');
-const userData = require('./app');
+const mainApp = require('./app');
 pg.defaults.ssl = true;
 
 
-sendTextMessage(945859885542482,'This is a test for schedule day one.');
+mainApp.sendTextMessage(945859885542482,'This is a test for schedule day one.');
 console.log("Sending scheduled message");
