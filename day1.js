20f716b8334c11d830c76903a409b7b409f6fbc4
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-06 07:38:45 -0400

    ...

--- day1.js
+++ day1.js
@@ -4,6 +4,6 @@ const config = require("./config");
 const uuid = require('uuid');
 const mainApp = require("./app");
 
-var quick_replies = ["ZeKh34suW"];
+var quick_replies = "ZeKh34suW";
 
-mainApp.sendQuickReply(945859885542482,'This is a test.',quick_replies);
+mainApp.sendQuickReply("945859885542482",'This is a test.',quick_replies);
