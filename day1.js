a296e4af0b4b17fb59fa6c3913e35916e6dc7e91
Eric Cadieux-Seney <eric.c@alpacode.com>
2017-10-06 08:08:30 -0400

    ...

--- day1.js
+++ day1.js
@@ -2,8 +2,8 @@
 'use strict';
 const config = require("./config");
 const uuid = require('uuid');
-require("./app");
+const app = require("./app");
 
 var quick_replies = "ZeKh34suW";
 
-sendQuickReply("945859885542482",'This is a test.',quick_replies);
+app.sendQuickReply("945859885542482",'This is a test.',quick_replies);
